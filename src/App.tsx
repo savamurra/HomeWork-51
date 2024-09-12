418218ece5cb4a4cbe6c2059992c0f0fa4c97821
Sanzhar Beishekeev <franki-55@mail.ru>
2024-09-12 16:39:26 +0600

    Написал функцию для получения рандомных чисел при нажатии на кнопку без повторений чисел

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import './App.css';
 import Numbers from './components/Number/Numbers.tsx';
-import {useState} from "react";
+import {useState} from 'react';
 
 
 const App = () => {
-  const [num] = useState<number[]>([5, 11, 16 ,23, 32])
+  const [num, setNum] = useState<number[]>([5, 11, 16 ,23, 32])
+
+  const randomNumber = (): void => {
+    let array: number[] = [];
+    let countCycles = 0;
+
+    for (let i = 5; i <= 36; i++)
+      array.push(i);
+    console.log(array);
+
+    let newArrayWithRandomNumber: number[] = [];
+
+    for (countCycles = 1; countCycles <= 5; countCycles++){
+      let numberIndex = array.splice(Math.random()*array.length, 1)[0];
+      newArrayWithRandomNumber.push(numberIndex);
+    }
+    setNum(newArrayWithRandomNumber);
+  }
 
   const numbersFromArray = num.map((number, index) =>{
     return (
@@ -14,7 +31,7 @@ const App = () => {
   return (
       <>
         {numbersFromArray}
-        <button className="random-btn" type="button">New Numbers</button>
+        <button onClick={randomNumber} className="random-btn" type="button">New Numbers</button>
       </>
   );
 };
